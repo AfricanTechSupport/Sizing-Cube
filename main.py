717f04160d88ce180d83ab1702962ccd32c1232a
Alan <obvicechief@gmail.com>
2022-03-01 10:00:32 -0600

    Update main.blocks, main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+def on_logo_long_pressed():
+    onLogoPress()
+input.on_logo_event(TouchButtonEvent.LONG_PRESSED, on_logo_long_pressed)
+
 def cubePattern():
     basic.show_leds("""
         # # # # #
@@ -47,19 +51,22 @@ def cubePattern():
                 # # # # #
     """)
 def playMelodies():
-    music.play_melody("F D C5 A E G C B ", 120)
+    music.play_melody("C C E A F G C C ", 200)
+def onLogoPress():
+    basic.show_icon(IconNames.CHESSBOARD)
 
 def on_button_pressed_ab():
     basic.clear_screen()
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 basic.show_string("Sizing Cube")
+basic.show_string("Enjoy!")
 
 def on_forever():
     cubePattern()
 basic.forever(on_forever)
 
 def on_in_background():
-    for index in range(1000000):
+    for index in range(1e+45):
         playMelodies()
 control.in_background(on_in_background)
